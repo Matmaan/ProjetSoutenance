466dffcfedc4be598dce554199bf9db3e93043a9
Mathieu Briatte <briattemathieu@yahoo.fr>
2017-05-04 15:44:36 +0200

    fix

--- app/Views/default/camp.php
+++ app/Views/default/camp.php
@@ -19,10 +19,21 @@
                 <h5 class="mt-0 text-left"><strong>Camp de bûcheron</strong> Niveau : <?php echo $_SESSION["buildings"]->wood_farm; ?></h5>
 
                 Le camp de bûcheron vous permettra d'enrichir votre camp en bois, une ressource essentielle à la survie de votre camp. 
+                <br>
                 <p class="text-right">Prix : <?php echo $bucheron->GetPrixBois(); ?> bois</p>
 
                 <?php if(empty($_SESSION["construct"]->wood_farm)):?>
                     <?php if($bucheron->action == 1): ?>
+                        <p style="text-align: right;"> 
+                        <?php
+                            $secondes = $bucheron->GetTemps();
+                            $temp = $secondes % 3600;
+                            $heures = ( $secondes - $temp ) / 3600 ;
+                            $secondes = $temp % 60 ;
+                            $minutes = ( $temp - $secondes ) / 60; 
+                            echo "Temps de construction : " . $heures . "h" . $minutes . "m" . $secondes . "s"; 
+                        ?>
+                        </p>
                         <a class='btn' href='<?=$this->url('default_upgrade',['idBuilding'=>2])?>'><img src="<?= $this->assetUrl('/img/button.png'); ?>" alt=""></a>
                     <?php endif ?>
                 <?php else:?>
@@ -40,12 +51,23 @@
                 <h5 class="mt-0 text-left"><strong>Ferme</strong> Niveau : <?php echo $_SESSION["buildings"]->food_farm; ?></h5>
 
                 Comment survivre sans manger ? Impossible ! Cette ferme vous aidera à faire prospérer votre campement dans la durée.
+                <br>
 
-                <p>Prix : <?php echo $ferme->GetPrixBois(); ?> Bois <?php echo $ferme->GetPrixNourriture(); ?> nourriture</p>
+                <p class="text-right">Prix : <?php echo $ferme->GetPrixBois(); ?> Bois <?php echo $ferme->GetPrixNourriture(); ?> nourriture</p>
 
 
                 <?php if(empty($_SESSION["construct"]->food_farm)):?>
                     <?php if($ferme->action == 1): ?>
+                        <p style="text-align: right;"> 
+                        <?php
+                            $secondes = $ferme->GetTemps();
+                            $temp = $secondes % 3600;
+                            $heures = ( $secondes - $temp ) / 3600 ;
+                            $secondes = $temp % 60 ;
+                            $minutes = ( $temp - $secondes ) / 60; 
+                            echo "Temps de construction : " . $heures . "h" . $minutes . "m" . $secondes . "s"; 
+                        ?>
+                        </p>
                         <a class='btn' href='<?=$this->url('default_upgrade',['idBuilding'=>3])?>'><img src="<?= $this->assetUrl('/img/button.png'); ?>" alt=""></a>
                     <?php endif ?>
                 <?php else:?>
@@ -64,13 +86,24 @@
                     <h5 class="mt-0 text-left"><strong>Puit</strong> Niveau : <?php echo $_SESSION["buildings"]->water_farm; ?></h5>
                     
                     La base de la vie, l'eau ! Evidemment elle est tellement importante qu'elle est très rare. Il faut donc l'utiliser à bon escient.
+                    <br>
 
-                    <p>Prix : <?php echo $puit->GetPrixBois(); ?> bois <?php echo $puit->GetPrixNourriture(); ?> nourriture <?php echo $puit->GetPrixEau(); ?> eau</p>
+                    <p class="text-right">Prix : <?php echo $puit->GetPrixBois(); ?> bois <?php echo $puit->GetPrixNourriture(); ?> nourriture <?php echo $puit->GetPrixEau(); ?> eau</p>
 
 
 
                     <?php if(empty($_SESSION["construct"]->water_farm)):?>
                         <?php if($puit->action == 1): ?>
+                            <p style="text-align: right;"> 
+                        <?php
+                            $secondes = $puit->GetTemps();
+                            $temp = $secondes % 3600;
+                            $heures = ( $secondes - $temp ) / 3600 ;
+                            $secondes = $temp % 60 ;
+                            $minutes = ( $temp - $secondes ) / 60; 
+                            echo "Temps de construction : " . $heures . "h" . $minutes . "m" . $secondes . "s"; 
+                        ?>
+                        </p>
                             <a class='btn' href='<?=$this->url('default_upgrade',['idBuilding'=>4])?>'><img src="<?= $this->assetUrl('/img/button.png'); ?>" alt=""></a>
                         <?php endif ?>
                     <?php else:?>
@@ -92,12 +125,23 @@
                 <h5 class="mt-0 text-left"><strong>Hangar</strong> Niveau : <?php echo $_SESSION["buildings"]->wood_stock; ?></h5>
 
                 Dans un monde post-apocalyptique il est évident que la richesse et le pouvoir passe par possession de ressource essentiel.
+                <br>
 
-                <p>Prix : <?php echo $hangar->GetPrixBois(); ?> bois</p>
+                <p class="text-right">Prix : <?php echo $hangar->GetPrixBois(); ?> bois</p>
 
 
                 <?php if(empty($_SESSION["construct"]->wood_stock)):?>
                     <?php if($hangar->action == 1): ?>
+                        <p style="text-align: right;"> 
+                        <?php
+                            $secondes = $hangar->GetTemps();
+                            $temp = $secondes % 3600;
+                            $heures = ( $secondes - $temp ) / 3600 ;
+                            $secondes = $temp % 60 ;
+                            $minutes = ( $temp - $secondes ) / 60; 
+                            echo "Temps de construction : " . $heures . "h" . $minutes . "m" . $secondes . "s"; 
+                        ?>
+                        </p>
                         <a class='btn' href='<?=$this->url('default_upgrade',['idBuilding'=>5])?>'><img src="<?= $this->assetUrl('/img/button.png'); ?>" alt=""></a>
                     <?php endif ?>
                 <?php else:?>
@@ -119,11 +163,23 @@
                 <h5 class="mt-0 text-left"><strong>Garde manger</strong> Niveau : <?php echo $_SESSION["buildings"]->food_stock; ?></h5>
 
                 A défaut d'avoir un frigo et de l'électricité au bout du fil il faut prévoir un lieu pour stocker votre nourriture.
-                <p>Prix : <?php echo $garde_manger->GetPrixBois(); ?> Bois <?php echo $garde_manger->GetPrixNourriture(); ?> nourriture</p>
+
+                <br>
+                <p class="text-right">Prix : <?php echo $garde_manger->GetPrixBois(); ?> Bois <?php echo $garde_manger->GetPrixNourriture(); ?> nourriture</p>
 
 
                 <?php if(empty($_SESSION["construct"]->food_stock)):?>
                     <?php if($garde_manger->action == 1): ?>
+                        <p style="text-align: right;"> 
+                        <?php
+                            $secondes = $garde_manger->GetTemps();
+                            $temp = $secondes % 3600;
+                            $heures = ( $secondes - $temp ) / 3600 ;
+                            $secondes = $temp % 60 ;
+                            $minutes = ( $temp - $secondes ) / 60; 
+                            echo "Temps de construction : " . $heures . "h" . $minutes . "m" . $secondes . "s"; 
+                        ?>
+                        </p>
                         <a class='btn' href='<?=$this->url('default_upgrade',['idBuilding'=>6])?>'><img src="<?= $this->assetUrl('/img/button.png'); ?>" alt=""></a>
                     <?php endif ?>
                 <?php else:?>
@@ -143,13 +199,24 @@
                 <h5 class="mt-0 text-left"><strong>Citerne</strong> Niveau : <?php echo $_SESSION["buildings"]->water_stock; ?></h5>
 
                 L'eau peut être très vite contaminée par les zombies et les autre camps qui vous chercher à réduire votre camp à néant pour vous piller.
+                <br>
 
-                <p>Prix : <?php echo $citerne->GetPrixBois(); ?> bois <?php echo $citerne->GetPrixNourriture(); ?> nourriture <?php echo $citerne->GetPrixEau(); ?> eau</p>
+                <p class="text-right">Prix : <?php echo $citerne->GetPrixBois(); ?> bois <?php echo $citerne->GetPrixNourriture(); ?> nourriture <?php echo $citerne->GetPrixEau(); ?> eau</p>
 
 
 
                     <?php if(empty($_SESSION["construct"]->water_stock)):?>
                         <?php if($citerne->action == 1): ?>
+                            <p style="text-align: right;"> 
+                        <?php
+                            $secondes = $citerne->GetTemps();
+                            $temp = $secondes % 3600;
+                            $heures = ( $secondes - $temp ) / 3600 ;
+                            $secondes = $temp % 60 ;
+                            $minutes = ( $temp - $secondes ) / 60; 
+                            echo "Temps de construction : " . $heures . "h" . $minutes . "m" . $secondes . "s"; 
+                        ?>
+                        </p>
                             <a class='btn' href='<?=$this->url('default_upgrade',['idBuilding'=>7])?>'><img src="<?= $this->assetUrl('/img/button.png'); ?>" alt=""></a>
                         <?php endif ?>
                     <?php else:?>
@@ -172,13 +239,24 @@
                 <h5 class="mt-0 text-left"><strong>Cabanon</strong> Niveau : <?php echo $_SESSION["buildings"]->cabanon; ?></h5>
 
                 Après avoir bien manger et bien bu vous serez bien content de bien dormir et quoi de mieux qu'une cabane que vous rêviez de bâtir étant petit.
+                <br>
 
-                <p>Prix : <?php echo $cabane->GetPrixBois(); ?> bois <?php echo $cabane->GetPrixNourriture(); ?> nourriture <?php echo $cabane->GetPrixEau(); ?> eau</p>
+                <p class="text-right">Prix : <?php echo $cabane->GetPrixBois(); ?> bois <?php echo $cabane->GetPrixNourriture(); ?> nourriture <?php echo $cabane->GetPrixEau(); ?> eau</p>
 
 
 
                 <?php if(empty($_SESSION["construct"]->cabanon)):?>
                     <?php if($cabane->action == 1): ?>
+                        <p style="text-align: right;"> 
+                        <?php
+                            $secondes = $cabane->GetTemps();
+                            $temp = $secondes % 3600;
+                            $heures = ( $secondes - $temp ) / 3600 ;
+                            $secondes = $temp % 60 ;
+                            $minutes = ( $temp - $secondes ) / 60; 
+                            echo "Temps de construction : " . $heures . "h" . $minutes . "m" . $secondes . "s"; 
+                        ?>
+                        </p>
                         <a class='btn' href='<?=$this->url('default_upgrade',['idBuilding'=>8])?>'><img src="<?= $this->assetUrl('/img/button.png'); ?>" alt=""></a>
                     <?php endif ?>
                 <?php else:?>
@@ -198,13 +276,24 @@
                 <h5 class="mt-0 text-left"><strong>Station de radio</strong> Niveau : <?php echo $_SESSION["buildings"]->radio; ?></h5>
 
                 Plus on est de fous plus on rit ! Si vous espérez reconstruire l'humanité il va vous falloir de l'aider et pour avoir de l'aide il faut appeler à l'aide non ?
+                <br>
 
-                <p>Prix : <?php echo $radio->GetPrixBois(); ?> bois <?php echo $radio->GetPrixNourriture(); ?> nourriture <?php echo $radio->GetPrixEau(); ?> eau</p>
+                <p class="text-right">Prix : <?php echo $radio->GetPrixBois(); ?> bois <?php echo $radio->GetPrixNourriture(); ?> nourriture <?php echo $radio->GetPrixEau(); ?> eau</p>
 
 
 
                 <?php if(empty($_SESSION["construct"]->radio)):?>
                     <?php if($radio->action == 1): ?>
+                        <p style="text-align: right;"> 
+                        <?php
+                            $secondes = $radio->GetTemps();
+                            $temp = $secondes % 3600;
+                            $heures = ( $secondes - $temp ) / 3600 ;
+                            $secondes = $temp % 60 ;
+                            $minutes = ( $temp - $secondes ) / 60; 
+                            echo "Temps de construction : " . $heures . "h" . $minutes . "m" . $secondes . "s"; 
+                        ?>
+                        </p>
                         <a class='btn' href='<?=$this->url('default_upgrade',['idBuilding'=>9])?>'><img src="<?= $this->assetUrl('/img/button.png'); ?>" alt=""></a>
                     <?php endif ?>
                 <?php else:?>
@@ -224,14 +313,25 @@
                 <h5 class="mt-0 text-left"><strong>Mur de defense</strong> Niveau : <?php echo $_SESSION["buildings"]->wall; ?></h5>
                 
                 Afin de vous protéger des attaques de zombies il vous faut une enceinte pour vous protéger. Quoi de mieux qu'un mur qui pourra retenir une partie des assauts de zombies.
+                <br>
 
-                <p>Prix : <?php echo $mur->GetPrixBois(); ?> bois <?php echo $mur->GetPrixNourriture(); ?> nourriture <?php echo $mur->GetPrixEau(); ?> eau</p>
+                <p class="text-right">Prix : <?php echo $mur->GetPrixBois(); ?> bois <?php echo $mur->GetPrixNourriture(); ?> nourriture <?php echo $mur->GetPrixEau(); ?> eau</p>
 
 
                
 
                 <?php if(empty($_SESSION["construct"]->wall)):?>
                     <?php if($mur->action == 1): ?>
+                        <p style="text-align: right;"> 
+                        <?php
+                            $secondes = $mur->GetTemps();
+                            $temp = $secondes % 3600;
+                            $heures = ( $secondes - $temp ) / 3600 ;
+                            $secondes = $temp % 60 ;
+                            $minutes = ( $temp - $secondes ) / 60; 
+                            echo "Temps de construction : " . $heures . "h" . $minutes . "m" . $secondes . "s"; 
+                        ?>
+                        </p>
                         <a class='btn' href='<?=$this->url('default_upgrade',['idBuilding'=>10])?>'><img src="<?= $this->assetUrl('/img/button.png'); ?>" alt=""></a>
                     <?php endif ?>
                 <?php else:?>
