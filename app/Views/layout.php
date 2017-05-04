9722df4c3b8fc0d122f4e366dc6ee7120cee5587
Mathieu Briatte <briattemathieu@yahoo.fr>
2017-05-04 13:44:05 +0200

    wiki

--- app/Views/layout.php
+++ app/Views/layout.php
@@ -32,6 +32,9 @@
 				<li <?= ($w_current_route == 'default_camp') ? 'class="active"' : ''; ?>><a href="<?php echo $this->url('default_camp'); ?>">Accueil</a></li>
 				<li <?= ($w_current_route == 'default_report') ? 'class="active"' : ''; ?>><a href="<?php echo $this->url('default_report'); ?>">Rapports<?=($_SESSION['newReport'] > 0)?"(".$_SESSION['newReport'].")":"";?></a></li>
 				<li <?= ($w_current_route == 'default_exploration') ? 'class="active"' : ''; ?>><a href="<?php echo $this->url('default_exploration'); ?>">Exploration</a></li>
+				<li <?= ($w_current_route == 'default_wiki') ? 'class="active"' : ''; ?>><a href="<?php echo $this->url('default_wiki'); ?>">Bien débuter</a></li>
+			
+
 			<?php else: ?>
 				<!-- Utilisateur non connecté -->
 				<li <?= ($w_current_route == 'user_login') ? 'class="active"' : ''; ?>><a href="<?php echo $this->url('user_login'); ?>">Accueil</a></li>
