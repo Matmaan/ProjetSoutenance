ec82ea68df6b54dbf5befb5c719cd0b896221a28
ryzommaster@hotmail.fr <ryzommaster@hotmail.fr>
2017-05-04 12:44:11 +0200

    Fix css

--- app/Views/default/report.php
+++ app/Views/default/report.php
@@ -12,7 +12,7 @@
                 </div><br>
             <?php endif; ?>
             <?php foreach ($reports as $key => $report): ?>
-                <div id="report_preview_<?=$report['id']?>" class="row report-preview <?=!($report['seen'])?"bg-warning":""?>" style="margin-right: 20px; margin-bottom: 20px;">
+                <div id="report_preview_<?=$report['id']?>" styclass="row report-preview <?=!($report['seen'])?"bg-warning":""?>" style="margin-right: 20px; margin-bottom: 20px;">
                     <p class="report-name"><?=$report['name']?></p>
                     <p class="report-date"><?=$report['report_date']?></p>
                     <button type="button" id="btn<?=$report['id']?>" class="btn btn-xs btn-default btn-report" name="button">Voir le rapport</button>
